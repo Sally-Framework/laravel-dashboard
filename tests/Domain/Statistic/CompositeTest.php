d4c2a6082990e4721fa1e7e6b33f1fb971506c87
n0tm <38406016+n0tm@users.noreply.github.com>
2020-03-15 22:56:52 +0300

    Установил phpUnit, сделал тест для Composite

---
+++ tests/Domain/Statistic/CompositeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Domain\Statistic;
+
+use PHPUnit\Framework\TestCase;
+
+class CompositeTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
