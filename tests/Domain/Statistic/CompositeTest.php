36d9157e3da3998a9870c63409a3338485726ba1
n0tm <38406016+n0tm@users.noreply.github.com>
2020-03-16 00:01:22 +0300

    Написал тест на Composite

--- tests/Domain/Statistic/CompositeTest.php
+++ tests/Domain/Statistic/CompositeTest.php
@@ -3,11 +3,32 @@
 namespace Tests\Domain\Statistic;
 
 use PHPUnit\Framework\TestCase;
+use Sally\Dashboard\Domain\Statistic\Composite;
+use Sally\Dashboard\Domain\Statistic\Type\AbstractType;
+use Sally\Dashboard\Domain\Statistic\Type\Text;
 
 class CompositeTest extends TestCase
 {
-    public function testExample()
+    /**
+     * @var Composite
+     */
+    private $composite;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        $this->composite = new Composite();
+    }
+
+    public function testGettingItemsAfterAdd()
+    {
+        $this->assertEmpty($this->composite->getItems());
+        $type = $this->getAbstractType();
+        $this->composite->add($type);
+        $this->assertSame([$type], $this->composite->getItems());
+    }
+
+    private function getAbstractType(): AbstractType
+    {
+        return new Text('::some name::', '::some value::');
     }
 }
